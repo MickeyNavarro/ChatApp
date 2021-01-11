d02106793f3d5dd3b0e5af38b7c8f074a347285d
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2021-01-11 16:16:19 -0700

    set up the websocket configuration to allow for messages to be sent

---
+++ spring-boot-websocket-ChatApp/src/main/java/com/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration 
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+	
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) { 
+		//allows for connection fallback if connection is down 
+		registry.addEndpoint("/chat").withSockJS();
+		//WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+	}
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) { 
+		//enables a simple message broker 
+		registry.enableSimpleBroker("/topic"); 
+		//assures that all messages will be received on the server based on the prefix
+		registry.setApplicationDestinationPrefixes("/app"); 
+		//WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+	}
+}
